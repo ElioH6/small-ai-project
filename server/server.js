7ed74a740ca2d9fbe986526bf932895ef232127e
ElioH6 <145810382+ElioH6@users.noreply.github.com>
2026-04-10 21:51:26 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,12 +10,14 @@ const port = process.env.PORT || 3000;
 
 app.use(cors({
   origin: [
-    "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://127.0.0.1:5500",
     "https://elioh6.github.io"
-  ]
+  ],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
 }));
-
+app.options("*", cors());
 app.use(express.json());
 
 if (!process.env.OPENROUTER_API_KEY) {
